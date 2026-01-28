fb080351852665d7e55bc0bcba5d6694c6c8c367
Sajid Rizvi <sajid97rizvi@gmail.com>
2026-01-28 20:59:58 +0530

    Changed file name to conform by pylint standards and added an adder which adds two integers

---
+++ new_python_file.py
@@ -0,0 +1,11 @@
+"""Prints something"""
+print("Hey this is test commit file + Now added something")
+
+def adder(a, b):
+    """Takes two integers and adds them together"""
+    return a + b
+
+RESULT = adder(5,8)
+
+"""Prints the result"""
+print(RESULT)
